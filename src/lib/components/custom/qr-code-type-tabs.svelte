bd5e2af6d3971959530aeab4976c4a0d46776421
Herman Brunberg <herman.brunberg@mio.se>
2025-05-30 12:16:27 +0200

    refactor: move tabs to QrCodeTypeTabs component

---
+++ src/lib/components/custom/qr-code-type-tabs.svelte
@@ -0,0 +1,129 @@
+<script lang="ts">
+	import { Input } from '$lib/components/ui/input';
+	import { Textarea } from '$lib/components/ui/textarea';
+	import * as Select from '$lib/components/ui/select';
+	import * as Tabs from '$lib/components/ui/tabs';
+	import { type QrCodeData, type QrCodeFormat } from '@/utils/qr-code-format';
+
+	type QrCodeTypeTabsProps = {
+		qrCodeFormat: QrCodeFormat;
+		qrCodeData: QrCodeData;
+	};
+
+	let { qrCodeFormat = $bindable(), qrCodeData = $bindable() }: QrCodeTypeTabsProps = $props();
+
+	const encryptions = [
+		{ value: 'WPA', label: 'WPA' },
+		{ value: 'WEP', label: 'WEP' },
+		{ value: 'nopass', label: 'No Password' }
+	];
+
+	const triggerContentEncryptions = $derived(
+		encryptions.find((e) => e.value === qrCodeData.wifi.encryption)?.label ?? 'Select an encryption'
+	);
+
+	$effect(() => {
+		qrCodeData.wifi.encryption === 'nopass' && (qrCodeData.wifi.password = '');
+	});
+</script>
+
+<Tabs.Root
+	value="url"
+	onValueChange={(value) => (qrCodeFormat = value as QrCodeFormat)}
+	class="flex w-full items-center justify-center"
+>
+	<Tabs.List>
+		<Tabs.Trigger value="url">Url</Tabs.Trigger>
+		<Tabs.Trigger value="wifi">Wifi</Tabs.Trigger>
+		<Tabs.Trigger value="text">Text</Tabs.Trigger>
+		<Tabs.Trigger value="email">Email</Tabs.Trigger>
+		<Tabs.Trigger value="phone">Phone</Tabs.Trigger>
+		<Tabs.Trigger value="sms">Sms</Tabs.Trigger>
+	</Tabs.List>
+	<Tabs.Content value="url" class="w-full sm:w-1/2">
+		<Input
+			class="mt-2"
+			type="url"
+			id="url"
+			placeholder="https://example.com"
+			bind:value={qrCodeData.url}
+		/>
+	</Tabs.Content>
+	<Tabs.Content value="wifi" class="w-full sm:w-1/2">
+		<Input
+			class="mt-2"
+			type="text"
+			id="ssid"
+			placeholder="SSID"
+			bind:value={qrCodeData.wifi.ssid}
+		/>
+		<div class="mt-2 flex w-full flex-row items-center justify-start gap-2">
+			<Select.Root type="single" name="encryption" bind:value={qrCodeData.wifi.encryption}>
+				<Select.Trigger class="w-[180px]">
+					{triggerContentEncryptions}
+				</Select.Trigger>
+				<Select.Content>
+					<Select.Group>
+						<Select.Label>Encryptions</Select.Label>
+						{#each encryptions as encryption (encryption.value)}
+							<Select.Item value={encryption.value} label={encryption.label}>
+								{encryption.label}
+							</Select.Item>
+						{/each}
+					</Select.Group>
+				</Select.Content>
+			</Select.Root>
+			{#if qrCodeData.wifi.encryption !== 'nopass'}
+				<Input
+					type="password"
+					id="password"
+					placeholder="Password"
+					bind:value={qrCodeData.wifi.password}
+				/>
+			{/if}
+		</div>
+	</Tabs.Content>
+	<Tabs.Content value="text" class="w-full sm:w-1/2">
+		<Input
+			class="mt-2"
+			type="text"
+			id="text"
+			placeholder="Enter text"
+			bind:value={qrCodeData.text}
+		/>
+	</Tabs.Content>
+	<Tabs.Content value="email" class="w-full sm:w-1/2">
+		<Input
+			class="mt-2"
+			type="email"
+			id="email"
+			placeholder="Enter email address"
+			bind:value={qrCodeData.email}
+		/>
+	</Tabs.Content>
+	<Tabs.Content value="phone" class="w-full sm:w-1/2">
+		<Input
+			class="mt-2"
+			type="tel"
+			id="phone"
+			placeholder="Enter phone number"
+			bind:value={qrCodeData.phone}
+		/>
+	</Tabs.Content>
+	<Tabs.Content value="sms" class="w-full sm:w-1/2">
+		<Input
+			required
+			class="mt-2"
+			type="tel"
+			id="sms"
+			placeholder="Enter phone number for SMS"
+			bind:value={qrCodeData.sms.phone}
+		/>
+		<Textarea
+			class="mt-2"
+			id="smsMessage"
+			placeholder="Enter SMS message"
+			bind:value={qrCodeData.sms.message}
+		/>
+	</Tabs.Content>
+</Tabs.Root>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,14 +1,11 @@
 <script lang="ts">
-	import { Input } from '$lib/components/ui/input';
 	import { Label } from '$lib/components/ui/label';
 	import { Button } from '@/components/ui/button';
 	import { Slider } from '$lib/components/ui/slider';
-	import { Textarea } from '$lib/components/ui/textarea';
 	import { Switch } from '$lib/components/ui/switch/index.js';
 	import * as Card from '$lib/components/ui/card';
 	import * as Collapsible from '$lib/components/ui/collapsible';
 	import * as Select from '$lib/components/ui/select';
-	import * as Tabs from '$lib/components/ui/tabs';
 	import DownloadIcon from '@lucide/svelte/icons/download';
 	import ChevronDownIcon from '@lucide/svelte/icons/chevron-down';
 	import ChevronUpIcon from '@lucide/svelte/icons/chevron-up';
@@ -16,6 +13,7 @@
 	import QRCode from '@castlenine/svelte-qrcode';
 	import { blur } from 'svelte/transition';
 	import { formatQrCodeData, type QrCodeData, type QrCodeFormat } from '@/utils/qr-code-format';
+	import QrCodeTypeTabs from '@/components/custom/qr-code-type-tabs.svelte';
 
 	let data = $state('https://qr-code-generator-seven-beryl.vercel.app');
 	let rendered = $state(false);
@@ -49,12 +47,6 @@
 		{ value: 'webp', label: 'webp' }
 	];
 
-	const encryptions = [
-		{ value: 'WPA', label: 'WPA' },
-		{ value: 'WEP', label: 'WEP' },
-		{ value: 'nopass', label: 'No Password' }
-	];
-
 	const shapes = [
 		{ value: 'square', label: 'Square' },
 		{ value: 'circle', label: 'Circle' }
@@ -62,10 +54,6 @@
 
 	const triggerContentFileFormat = $derived(
 		fileFormats.find((f) => f.value === downloadUrlFileFormat)?.label ?? 'Select a file format'
-	);
-
-	const triggerContentEncryptions = $derived(
-		encryptions.find((e) => e.value === qrCodeData.wifi.encryption)?.label ?? 'Select an encryption'
 	);
 
 	const triggerContentShapes = $derived(
@@ -102,106 +90,7 @@
 		</Card.Header>
 		<Card.Content class="flex w-full flex-col items-center justify-center gap-5">
 			<div class="flex w-full flex-col items-center justify-center">
-				<Tabs.Root
-					value="url"
-					onValueChange={(value) => (qrCodeFormat = value as QrCodeFormat)}
-					class="flex w-full items-center justify-center"
-				>
-					<Tabs.List>
-						<Tabs.Trigger value="url">Url</Tabs.Trigger>
-						<Tabs.Trigger value="wifi">Wifi</Tabs.Trigger>
-						<Tabs.Trigger value="text">Text</Tabs.Trigger>
-						<Tabs.Trigger value="email">Email</Tabs.Trigger>
-						<Tabs.Trigger value="phone">Phone</Tabs.Trigger>
-						<Tabs.Trigger value="sms">Sms</Tabs.Trigger>
-					</Tabs.List>
-					<Tabs.Content value="url" class="w-full sm:w-1/2">
-						<Input
-							class="mt-2"
-							type="url"
-							id="url"
-							placeholder="https://example.com"
-							bind:value={qrCodeData.url}
-						/>
-					</Tabs.Content>
-					<Tabs.Content value="wifi" class="w-full sm:w-1/2">
-						<Input
-							class="mt-2"
-							type="text"
-							id="ssid"
-							placeholder="SSID"
-							bind:value={qrCodeData.wifi.ssid}
-						/>
-						<div class="mt-2 flex w-full flex-row items-center justify-start gap-2">
-							<Select.Root type="single" name="encryption" bind:value={qrCodeData.wifi.encryption}>
-								<Select.Trigger class="w-[180px]">
-									{triggerContentEncryptions}
-								</Select.Trigger>
-								<Select.Content>
-									<Select.Group>
-										<Select.Label>Encryptions</Select.Label>
-										{#each encryptions as encryption (encryption.value)}
-											<Select.Item value={encryption.value} label={encryption.label}>
-												{encryption.label}
-											</Select.Item>
-										{/each}
-									</Select.Group>
-								</Select.Content>
-							</Select.Root>
-							{#if qrCodeData.wifi.encryption !== 'nopass'}
-								<Input
-									type="password"
-									id="password"
-									placeholder="Password"
-									bind:value={qrCodeData.wifi.password}
-								/>
-							{/if}
-						</div>
-					</Tabs.Content>
-					<Tabs.Content value="text" class="w-full sm:w-1/2">
-						<Input
-							class="mt-2"
-							type="text"
-							id="text"
-							placeholder="Enter text"
-							bind:value={qrCodeData.text}
-						/>
-					</Tabs.Content>
-					<Tabs.Content value="email" class="w-full sm:w-1/2">
-						<Input
-							class="mt-2"
-							type="email"
-							id="email"
-							placeholder="Enter email address"
-							bind:value={qrCodeData.email}
-						/>
-					</Tabs.Content>
-					<Tabs.Content value="phone" class="w-full sm:w-1/2">
-						<Input
-							class="mt-2"
-							type="tel"
-							id="phone"
-							placeholder="Enter phone number"
-							bind:value={qrCodeData.phone}
-						/>
-					</Tabs.Content>
-					<Tabs.Content value="sms" class="w-full sm:w-1/2">
-						<Input
-							required
-							class="mt-2"
-							type="tel"
-							id="sms"
-							placeholder="Enter phone number for SMS"
-							bind:value={qrCodeData.sms.phone}
-						/>
-						<Textarea
-							class="mt-2"
-							id="smsMessage"
-							placeholder="Enter SMS message"
-							bind:value={qrCodeData.sms.message}
-						/>
-					</Tabs.Content>
-				</Tabs.Root>
+				<QrCodeTypeTabs bind:qrCodeFormat bind:qrCodeData />
 			</div>
 			<div class="flex w-full flex-col items-center justify-center">
 				<Collapsible.Root class="w-full" bind:open={collapsibleOpen}>
