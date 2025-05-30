db86ec95fd4082e91aa933aad2cf0d8acb411356
Herman Brunberg <herman.brunberg@mio.se>
2025-05-30 12:37:30 +0200

    refactor: move advanced settings to QrCodeAdvancedSettings component

---
+++ src/lib/components/custom/qr-code-advanced-settings.svelte
@@ -0,0 +1,137 @@
+<script lang="ts">
+	import { Label } from '$lib/components/ui/label';
+	import { Slider } from '$lib/components/ui/slider';
+	import { Switch } from '$lib/components/ui/switch/index.js';
+	import * as Collapsible from '$lib/components/ui/collapsible';
+	import * as Select from '$lib/components/ui/select';
+	import ChevronDownIcon from '@lucide/svelte/icons/chevron-down';
+	import ChevronUpIcon from '@lucide/svelte/icons/chevron-up';
+	import ColorPicker from 'svelte-awesome-color-picker';
+	import { type QrCodeData } from '@/utils/qr-code-format';
+
+	export type AdvancedSettings = {
+		size: number;
+		shape: 'square' | 'circle';
+		haveBackgroundRoundedEdges: boolean;
+		haveGappedModules: boolean;
+		backgroundColor: string;
+		color: string;
+		downloadUrlFileFormat: 'png' | 'svg' | 'jpg' | 'jpeg' | 'webp' | undefined;
+	};
+
+	type QrCodeAdvancedSettingsProps = {
+		options: AdvancedSettings;
+		open: boolean;
+		data: QrCodeData;
+	};
+
+	let {
+		options = $bindable<AdvancedSettings>(),
+		open = $bindable(),
+		data = $bindable<QrCodeData>()
+	}: QrCodeAdvancedSettingsProps = $props();
+
+	const fileFormats = [
+		{ value: 'png', label: 'png' },
+		{ value: 'svg', label: 'svg' },
+		{ value: 'jpg', label: 'jpg' },
+		{ value: 'jpeg', label: 'jpeg' },
+		{ value: 'webp', label: 'webp' }
+	];
+
+	const shapes = [
+		{ value: 'square', label: 'Square' },
+		{ value: 'circle', label: 'Circle' }
+	];
+
+	const triggerContentFileFormat = $derived(
+		fileFormats.find((f) => f.value === options.downloadUrlFileFormat)?.label ??
+			'Select a file format'
+	);
+
+	const triggerContentShapes = $derived(
+		shapes.find((s) => s.value === options.shape)?.label ?? 'Select a shape'
+	);
+</script>
+
+<Collapsible.Root class="w-full" bind:open>
+	<Collapsible.Trigger class="w-full">
+		<div class="flex w-full cursor-pointer flex-row items-center justify-center">
+			Advanced settings
+			{#if open}
+				<ChevronUpIcon />
+			{:else}
+				<ChevronDownIcon />
+			{/if}
+		</div>
+	</Collapsible.Trigger>
+	<Collapsible.Content>
+		<div>
+			<Label for="size" class="mt-4">QR-Code Size</Label>
+			<div class="flex flex-row items-center justify-between gap-2 px-1">
+				<Slider id="size" type="single" bind:value={options.size} max={1000} step={1} />
+				<span class="text-muted-foreground text-sm">{options.size}px</span>
+			</div>
+		</div>
+		<div
+			class="mt-4 flex w-full flex-col items-start gap-4 sm:flex-row sm:items-center sm:justify-between"
+		>
+			<div class="flex flex-col items-center justify-start gap-2 md:flex-row">
+				<div class="flex flex-col gap-1.5">
+					<Label for="fileFormat">File Format</Label>
+					<Select.Root type="single" name="fileFormat" bind:value={options.downloadUrlFileFormat}>
+						<Select.Trigger class="w-[180px]">
+							{triggerContentFileFormat}
+						</Select.Trigger>
+						<Select.Content>
+							<Select.Group>
+								<Select.Label>File Formats</Select.Label>
+								{#each fileFormats as format (format.value)}
+									<Select.Item value={format.value} label={format.label}>
+										{format.label}
+									</Select.Item>
+								{/each}
+							</Select.Group>
+						</Select.Content>
+					</Select.Root>
+				</div>
+				<div class="flex flex-col gap-1.5">
+					<Label for="shape">Shape</Label>
+					<Select.Root type="single" name="shape" bind:value={options.shape}>
+						<Select.Trigger class="w-[180px]">
+							{triggerContentShapes}
+						</Select.Trigger>
+						<Select.Content>
+							<Select.Group>
+								<Select.Label>Shapes</Select.Label>
+								{#each shapes as shape (shape.value)}
+									<Select.Item value={shape.value} label={shape.label}>
+										{shape.label}
+									</Select.Item>
+								{/each}
+							</Select.Group>
+						</Select.Content>
+					</Select.Root>
+				</div>
+			</div>
+			<div class="flex w-full flex-col gap-4 rounded-md border p-3 sm:w-fit sm:flex-row">
+				<div>
+					<ColorPicker bind:hex={options.backgroundColor} label="Background Color" />
+				</div>
+				<div>
+					<ColorPicker bind:hex={options.color} label="Foreground Color" />
+				</div>
+			</div>
+		</div>
+		<div class="mt-4 flex flex-col">
+			<div class="mt-4 flex items-center space-x-2">
+				<Switch id="rounded" bind:checked={options.haveBackgroundRoundedEdges} />
+				<Label for="rounded">Rounded</Label>
+			</div>
+			<div class="mt-4 flex items-center space-x-2">
+				<Switch id="gapped-modules" bind:checked={options.haveGappedModules} />
+				<Label for="gapped-modules">Gapped Modules</Label>
+			</div>
+		</div>
+	</Collapsible.Content>
+</Collapsible.Root>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,31 +1,19 @@
 <script lang="ts">
-	import { Label } from '$lib/components/ui/label';
 	import { Button } from '@/components/ui/button';
-	import { Slider } from '$lib/components/ui/slider';
-	import { Switch } from '$lib/components/ui/switch/index.js';
 	import * as Card from '$lib/components/ui/card';
-	import * as Collapsible from '$lib/components/ui/collapsible';
-	import * as Select from '$lib/components/ui/select';
 	import DownloadIcon from '@lucide/svelte/icons/download';
-	import ChevronDownIcon from '@lucide/svelte/icons/chevron-down';
-	import ChevronUpIcon from '@lucide/svelte/icons/chevron-up';
-	import ColorPicker from 'svelte-awesome-color-picker';
 	import QRCode from '@castlenine/svelte-qrcode';
 	import { blur } from 'svelte/transition';
 	import { formatQrCodeData, type QrCodeData, type QrCodeFormat } from '@/utils/qr-code-format';
 	import QrCodeTypeTabs from '@/components/custom/qr-code-type-tabs.svelte';
+	import QrCodeAdvancedSettings, {
+		type AdvancedSettings
+	} from '@/components/custom/qr-code-advanced-settings.svelte';
 
 	let data = $state('https://qr-code-generator-seven-beryl.vercel.app');
 	let rendered = $state(false);
 	let downloadUrl = $state('');
-	let size = $state(256);
-	let shape: 'square' | 'circle' = $state('square');
-	let haveBackgroundRoundedEdges = $state(false);
-	let haveGappedModules = $state(false);
-	let backgroundColor = $state('#FFFFFF');
-	let color = $state('#000000');
-	let downloadUrlFileFormat: 'png' | 'svg' | 'jpg' | 'jpeg' | 'webp' | undefined = $state('png');
-	let collapsibleOpen = $state(false);
+	let advancedOptionsOpen = $state(false);
 	let qrCodeFormat: QrCodeFormat = $state('url');
 	let qrCodeData: QrCodeData = $state({
 		wifi: {
@@ -39,36 +27,19 @@
 		}
 	});
 
-	const fileFormats = [
-		{ value: 'png', label: 'png' },
-		{ value: 'svg', label: 'svg' },
-		{ value: 'jpg', label: 'jpg' },
-		{ value: 'jpeg', label: 'jpeg' },
-		{ value: 'webp', label: 'webp' }
-	];
-
-	const shapes = [
-		{ value: 'square', label: 'Square' },
-		{ value: 'circle', label: 'Circle' }
-	];
-
-	const triggerContentFileFormat = $derived(
-		fileFormats.find((f) => f.value === downloadUrlFileFormat)?.label ?? 'Select a file format'
-	);
-
-	const triggerContentShapes = $derived(
-		shapes.find((s) => s.value === shape)?.label ?? 'Select a shape'
-	);
+	let options = $state<AdvancedSettings>({
+		size: 256,
+		shape: 'square',
+		haveBackgroundRoundedEdges: false,
+		haveGappedModules: true,
+		backgroundColor: '#FFFFFF',
+		color: '#000000',
+		downloadUrlFileFormat: 'png'
+	});
 
 	const handleDownloadUrlGenerated = (url = '') => {
 		downloadUrl = url;
 	};
-
-	$effect(() => {
-		if (qrCodeData.wifi.encryption === 'nopass') {
-			qrCodeData.wifi.password = '';
-		}
-	});
 
 	const renderQrCode = () => {
 		const formattedData = formatQrCodeData(qrCodeData, qrCodeFormat);
@@ -93,87 +64,11 @@
 				<QrCodeTypeTabs bind:qrCodeFormat bind:qrCodeData />
 			</div>
 			<div class="flex w-full flex-col items-center justify-center">
-				<Collapsible.Root class="w-full" bind:open={collapsibleOpen}>
-					<Collapsible.Trigger class="w-full">
-						<div class="flex w-full cursor-pointer flex-row items-center justify-center">
-							Advanced settings
-							{#if collapsibleOpen}
-								<ChevronUpIcon />
-							{:else}
-								<ChevronDownIcon />
-							{/if}
-						</div>
-					</Collapsible.Trigger>
-					<Collapsible.Content>
-						<div>
-							<Label for="size" class="mt-4">QR-Code Size</Label>
-							<div class="flex flex-row items-center justify-between gap-2 px-1">
-								<Slider id="size" type="single" bind:value={size} max={1000} step={1} />
-								<span class="text-muted-foreground text-sm">{size}px</span>
-							</div>
-						</div>
-						<div
-							class="mt-4 flex w-full flex-col items-start gap-4 sm:flex-row sm:items-center sm:justify-between"
-						>
-							<div class="flex flex-col items-center justify-start gap-2 md:flex-row">
-								<div class="flex flex-col gap-1.5">
-									<Label for="fileFormat">File Format</Label>
-									<Select.Root type="single" name="fileFormat" bind:value={downloadUrlFileFormat}>
-										<Select.Trigger class="w-[180px]">
-											{triggerContentFileFormat}
-										</Select.Trigger>
-										<Select.Content>
-											<Select.Group>
-												<Select.Label>File Formats</Select.Label>
-												{#each fileFormats as format (format.value)}
-													<Select.Item value={format.value} label={format.label}>
-														{format.label}
-													</Select.Item>
-												{/each}
-											</Select.Group>
-										</Select.Content>
-									</Select.Root>
-								</div>
-								<div class="flex flex-col gap-1.5">
-									<Label for="shape">Shape</Label>
-									<Select.Root type="single" name="shape" bind:value={shape}>
-										<Select.Trigger class="w-[180px]">
-											{triggerContentShapes}
-										</Select.Trigger>
-										<Select.Content>
-											<Select.Group>
-												<Select.Label>Shapes</Select.Label>
-												{#each shapes as shape (shape.value)}
-													<Select.Item value={shape.value} label={shape.label}>
-														{shape.label}
-													</Select.Item>
-												{/each}
-											</Select.Group>
-										</Select.Content>
-									</Select.Root>
-								</div>
-							</div>
-							<div class="flex w-full flex-col gap-4 rounded-md border p-3 sm:w-fit sm:flex-row">
-								<div>
-									<ColorPicker bind:hex={backgroundColor} label="Background Color" />
-								</div>
-								<div>
-									<ColorPicker bind:hex={color} label="Foreground Color" />
-								</div>
-							</div>
-						</div>
-						<div class="mt-4 flex flex-col">
-							<div class="mt-4 flex items-center space-x-2">
-								<Switch id="rounded" bind:checked={haveBackgroundRoundedEdges} />
-								<Label for="rounded">Rounded</Label>
-							</div>
-							<div class="mt-4 flex items-center space-x-2">
-								<Switch id="gapped-modules" bind:checked={haveGappedModules} />
-								<Label for="gapped-modules">Gapped Modules</Label>
-							</div>
-						</div>
-					</Collapsible.Content>
-				</Collapsible.Root>
+				<QrCodeAdvancedSettings
+					bind:options
+					bind:data={qrCodeData}
+					bind:open={advancedOptionsOpen}
+				/>
 			</div>
 		</Card.Content>
 		<Card.Footer>
@@ -190,13 +85,13 @@
 					<span class="hidden">
 						<QRCode
 							bind:data
-							bind:size
-							bind:backgroundColor
-							bind:color
-							bind:downloadUrlFileFormat
-							bind:shape
-							bind:haveBackgroundRoundedEdges
-							bind:haveGappedModules
+							bind:size={options.size}
+							bind:backgroundColor={options.backgroundColor}
+							bind:color={options.color}
+							bind:downloadUrlFileFormat={options.downloadUrlFileFormat}
+							bind:shape={options.shape}
+							bind:haveBackgroundRoundedEdges={options.haveBackgroundRoundedEdges}
+							bind:haveGappedModules={options.haveGappedModules}
 							dispatchDownloadUrl
 							on:downloadUrlGenerated={(event) => handleDownloadUrlGenerated(event.detail.url)}
 						/>
@@ -208,7 +103,7 @@
 						<Button
 							class=""
 							href={downloadUrl}
-							download={`qr-code.${downloadUrlFileFormat}`}
+							download={`qr-code.${options.downloadUrlFileFormat}`}
 							target="_blank"
 						>
 							<DownloadIcon class="mr-2 size-4" />
